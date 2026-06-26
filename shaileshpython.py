8c370eaee2c24003e904d4f1f38f964d342cbfe0
shaileshjangid7 <shailesh.jangid@heromotocorp.com>
2026-06-26 14:55:14 +0530

    Update shaileshpython.py

--- shaileshpython.py
+++ shaileshpython.py
@@ -2,11 +2,11 @@ import streamlit as st
 from google import genai
 from google.genai import types
 config = types.GenerateContentConfig(
-    system_instruction = "You are an expert Python developer.
+    system_instruction = """You are an expert Python developer.
  Answer only questions related to Python programming.
  For any non-Python question, reply exactly:
  Please ask a Python-related question.
- Do not answer questions outside the Python domain."
+ Do not answer questions outside the Python domain."""
   )
 st.markdown(
   """
