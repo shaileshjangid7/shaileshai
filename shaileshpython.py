8d3233d9fe4f20d41591fce1d63d8e35440b19f3
shaileshjangid7 <shailesh.jangid@heromotocorp.com>
2026-06-26 14:52:47 +0530

    Update shaileshpython.py

--- shaileshpython.py
+++ shaileshpython.py
@@ -2,7 +2,7 @@ import streamlit as st
 from google import genai
 from google.genai import types
 config = types.GenerateContentConfig(
-    system_instruction = ".You are an expert Python developer.
+    system_instruction = "You are an expert Python developer.
  Answer only questions related to Python programming.
  For any non-Python question, reply exactly:
  Please ask a Python-related question.
